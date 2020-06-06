77bee6de85fef1368889378f3394a42e9db6525b
matematyk60 <dawid.gdk@gmail.com>
2020-06-06 14:58:00 +0200

    chore: add more logic to positioner

--- simulation/positioner.py
+++ simulation/positioner.py
@@ -14,42 +14,50 @@ class PositionRequest:
 
 @dataclass
 class PositionResponse:
-    next_way: int
+    next_way: Way
     next_position: int
     next_lane: int
     distance_travelled: int
     should_break: bool
     change_lane_to: Optional[bool]
 
-def position(position_request: PositionRequest) -> PositionResponse:
-    current_way = position_request.current_way
-    current_position = position_request.current_position
-    should_travel_to_next_way = True
-    collisioned = False
-    travelled_distance = 0
-    while should_travel_to_next_way:
-        for x in range(current_position + 1, current_way.distance):
-            if travelled_distance == position_request.speed:
-                should_travel_to_next_way = False
-            elif current_way.occupations[x].occupied:
-                should_travel_to_next_way = False
+class Positioner:
+    def __init__(self, way_dict: Dict[int, Way]):
+        self.way_dict = way_dict
+
+    def position(self, position_request: PositionRequest) -> PositionResponse:
+        current_way = position_request.current_way
+        current_position = position_request.current_position
+        should_travel_to_next_way = True
+        collisioned = False
+        travelled_distance = 0
+        while should_travel_to_next_way:
+            for x in range(current_position + 1, current_way.distance):
+                if travelled_distance == position_request.speed:
+                    should_travel_to_next_way = False
+                elif current_way.occupations[x].occupied:
+                    should_travel_to_next_way = False
+                    collisioned = True
+                    break
+                else:
+                    travelled_distance += 1
+                    current_position += 1
+            next_way = self.way_dict[position_request.directions_map[current_way.way_id]]
+            first_position_in_next_way_occupied = next_way.occupations[0].occupied
+            if should_travel_to_next_way and not first_position_in_next_way_occupied:
+                current_way = next_way
+                current_position = 0
+            elif should_travel_to_next_way and first_position_in_next_way_occupied:
                 collisioned = True
-                break
-            else:
-                travelled_distance += 1
-                current_position += 1
-        next_way = position_request.directions_map[current_way.way_id]
-        if travelled_distance < position_request.speed and not collisioned and not next_way.occupations[x].occupied:
-            current_way = next_way
-            current_position = 0
-    return PositionResponse(
-        next_way=current_way,
-        next_position=current_position,
-        next_lane=position_request.current_lane_number,
-        distance_travelled=travelled_distance,
-        should_break=collisioned,
-        change_lane_to=None
-    )
+                should_travel_to_next_way = False
+        return PositionResponse(
+            next_way=current_way,
+            next_position=current_position,
+            next_lane=position_request.current_lane_number,
+            distance_travelled=travelled_distance,
+            should_break=collisioned,
+            change_lane_to=None
+        )
 
 
 
